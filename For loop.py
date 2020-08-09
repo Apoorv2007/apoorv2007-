ede5a93ab3e4a708f46b532246027cdfd78fed86
Apoorv2007 <68950480+Apoorv2007@users.noreply.github.com>
2020-08-09 17:00:32 +0530

    Add files via upload

---
+++ For loop.py
@@ -0,0 +1,21 @@
+#for f in range (10):
+#print (f,end=" ")
+#for x in range (5,51,5):
+   # print("the loop value is ",x)
+   #for 0 to 6 traingle
+#for x in range (6):
+    
+    #print (list(x))
+    #for y in range (x):
+      # print (y,end=" ")
+    #print()
+ #for opposite
+for x in range (0,6):
+    for y in range (x,5):
+        print (end=" ")
+
+    
+    #print (list(x))
+    
+       
+    #print()
